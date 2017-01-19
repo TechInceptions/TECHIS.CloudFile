7faf6cab3e609129386655643ae623e0ee15b7b9
Arinze <arinze@techinceptions.com>
2017-01-19 15:50:47 -0500

    minor change to assembly info file

--- TECHIS.CloudFile/Properties/AssemblyInfo.cs
+++ TECHIS.CloudFile/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TECHIS.CloudFile")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("class interfaces for accessing files stored in cloud platforms like AWS or Azure")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TECH-IS INC.")]
 [assembly: AssemblyProduct("TECHIS.CloudFile")]
